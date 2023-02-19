2fd5ae277fcbd4e7a6dda95c6dd62646a49fd66d
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2023-02-19 22:57:37 +0300

    Ajout de Service user qui sera un class d'appel d'injection

---
+++ DEVELOPMENTS/Backend/Backend.Entity/Services/UserReq.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Entity.Services
+{
+    public class UserReq
+    {
+        public virtual string User_FirstName { get; set; }
+        public virtual string User_LastName { get; set; }
+        public virtual string User_CIN { get; set; }
+        public virtual DateTime User_BirthDay { get; set; }
+        public virtual bool User_Genre { get; set; }
+    }
+}
