c919588c2692510f5eb63324a06222fb1743bc41
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2023-02-19 22:54:42 +0300

    Mapping de User et Entity

---
+++ DEVELOPMENTS/Backend/Backend.Entity/Mapping/UserMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Entity.Mapping
+{
+    class UserMap : SubclassMap<User>
+    {
+        public UserMap()
+        {
+            Abstract();
+            Map(x => x.User_FirstName).Not.Nullable();
+            Map(x => x.User_LastName).Not.Nullable();
+            Map(x => x.User_CIN).Not.Nullable();
+            Map(x => x.User_BirthDay).Not.Nullable();
+            Map(x => x.User_Genre).Not.Nullable();
+        }
+    }
+}
